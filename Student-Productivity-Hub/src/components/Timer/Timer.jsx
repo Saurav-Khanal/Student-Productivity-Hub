abff142960e8afe7589b93f2a30b20e23101d29a
Sauravc-Dev <SAURAVKHANAL842@GMAIL.COM>
2026-08-05 12:57:22 +0545

    feat: complete pomodoro timer functionality

--- Student-Productivity-Hub/src/components/Timer/Timer.jsx
+++ Student-Productivity-Hub/src/components/Timer/Timer.jsx
@@ -36,13 +36,13 @@ const Timer = () => {
     setTime(inital_time)
   }
   return (
-    <div className="bg-white rounded-xl p-10 shadow-[0_20px_60px_rgba(0,0,0,0.25)] hover:-translate-y-2 transition-all duration-300 flex-1">
+    <div className="bg-white rounded-xl p-10 shadow-[0_20px_60px_rgba(0,0,0,0.25)] hover:-translate-y-2 transition-all duration-300 flex-1 m-40">
         <h1 className='text-3xl font-bold text-center'>Pomodoro Timer</h1>
         <h2 className='text-7xl font-bold text-center mt-8'>{minutes}:{formattedSeconds}</h2>
         <div className='flex justify-center gap-4 mt-8'>
-          <button onClick={startTimer}>Start</button>
-          <button onClick={pauseTImer}>Pause</button>
-          <button onClick={resetTimer}>Reset</button>
+          <button className='bg-green-500 hover:bg-green-600  px-5 py-2 rounded-lg text-white font-medium transition duration-200 shadow-md hover:scale-105 active:scale-95' onClick={startTimer}>Start</button>
+          <button className="bg-yellow-500 hover:bg-yellow-600 px-5 py-2 rounded-lg text-white font-medium transition duration-200 shadow-md hover:scale-105 active:scale-95 " onClick={pauseTImer}>Pause</button>
+          <button className="bg-red-500 hover:bg-red-600 px-5 py-2 rounded-lg text-white font-medium transition duration-200 shadow-md hover:scale-105 active:scale-95" onClick={resetTimer}>Reset</button>
         </div>
       </div>
   )
