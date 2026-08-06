7af4a477b48575289667f2e2aa83d759fc507c56
Sauravc-Dev <SAURAVKHANAL842@GMAIL.COM>
2026-08-06 14:29:10 +0545

    feat: add settings page with persistent timer configuration

--- Student-Productivity-Hub/src/App.jsx
+++ Student-Productivity-Hub/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <div className="flex h-screen overflow-hidden">
   <Sidebar />
   <main className="flex-1 overflow-y-auto p-6">
-    <Notes />
+    <Settings/>
   </main>
 </div>
   )
--- Student-Productivity-Hub/src/components/Settings/Settings.jsx
+++ Student-Productivity-Hub/src/components/Settings/Settings.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 const Settings = () => {
-  return (
-    <div>
-      
+  useEffect(()=>{
+   const savedPomo= localStorage.getItem("pomo");
+    const savedShort=localStorage.getItem("short");
+    const savedLong=localStorage.getItem("long");
+    setPomo(savedPomo);
+    setShort(savedShort);
+    setLong(savedLong)
+  },[])
+  const [pomo, setPomo] = useState(2);
+  const [short, setShort] = useState(4);
+  const [long, setLong] = useState(3);
+  const PomoHandler=(ele)=>{
+    setPomo(ele);
+  }
+
+  const ShortHandler=(value)=>{
+    setShort(value);
+  }
+
+  const LongHandler=(value)=>{
+    setLong(value);
+  }
+
+  const saveSetting=()=>{
+    localStorage.setItem("pomo",pomo);
+    localStorage.setItem("short",short);
+    localStorage.setItem("long",long);
+  }
+  
+  return(
+    <div className='flex-1'>
+      <div className='bg-white rounded-xl shadow-lg p-6'>
+      <h2 className='text-2xl font-bold'>Settings</h2>
+    <div className='mt-6'>
+      <label className='font-medium'>
+        Pomodoro Duration (minutes)
+      </label>
+      <input
+      value={pomo}
+      onChange={(ele)=>{
+        PomoHandler(ele.target.value);
+      }}
+      type='number'
+      className='w-full border rounded-lg p-3 mt-2 outline-none focus:ring focus:ring-blue-500'
+      />
+      </div>
+      <div className='mt-6'>
+      <label className='block font-medium'>
+        Short break (minutes)
+      </label>
+      <input
+      value={short}
+      onChange={(ele)=>{
+        ShortHandler(ele.target.value);
+      }}
+      type='number'
+      className='w-full border rounded-lg p-3 mt-2 outline-none focus:ring focus:ring-blue-500'
+      />
+      </div>
+      <div className='mt-6'>
+      <label className='block font-medium'>
+        Long break (minutes)
+      </label>
+      <input
+      value={long}
+      onChange={(ele)=>{
+        LongHandler(ele.target.value);
+      }}
+      type='number'
+      className='w-full border rounded-lg p-3 mt-2 outline-none focus:ring focus:ring-blue-500'
+      />
+      </div>
+      <button className='bg-blue-500 hover:bg-blue-600 transition text-white font-medium rounded-lg px-5 py-3 mt-6 w-full '
+      onClick={()=>{
+        saveSetting();
+      }}>
+        Save settings
+      </button>
+    </div>
     </div>
   )
 }
